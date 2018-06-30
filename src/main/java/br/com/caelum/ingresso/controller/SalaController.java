155c8935584603f09cb4a4295cba42823ff467eb
Marco Pontes <marcopontes@uol.com.br>
2018-06-30 11:31:13 -0300

    ex 2.6

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,16 +1,23 @@
 package br.com.caelum.ingresso.controller;
 
-import br.com.caelum.ingresso.dao.SalaDao;
-import br.com.caelum.ingresso.model.Sala;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
-import java.util.Optional;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sala;
 
 /**
  * Created by nando on 03/03/17.
@@ -20,8 +27,12 @@ public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
+    
+    @Autowired
+    private SessaoDao sessaoDao;
 
-
+    
+    
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
     public ModelAndView form(@PathVariable("id")Optional<Integer> id, Sala sala){
         ModelAndView modelAndView = new ModelAndView("sala/sala");
@@ -34,10 +45,9 @@ public class SalaController {
 
         return modelAndView;
     }
+    
 
-
-
-
+    
     @PostMapping("/admin/sala")
     @Transactional
     public ModelAndView salva(@Valid Sala sala, BindingResult result){
@@ -50,6 +60,8 @@ public class SalaController {
         return new ModelAndView("redirect:/admin/salas");
     }
 
+    
+    
     @GetMapping("/admin/salas")
     public ModelAndView lista(){
         ModelAndView modelAndView = new ModelAndView("sala/lista");
@@ -62,15 +74,18 @@ public class SalaController {
 
     @GetMapping("/admin/sala/{id}/sessoes")
     public ModelAndView listaSessoes(@PathVariable("id") Integer id) {
-
+	
         Sala sala = salaDao.findOne(id);
 
         ModelAndView view = new ModelAndView("sessao/lista");
         view.addObject("sala", sala);
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
 
         return view;
     }
 
+    
+    
     @GetMapping("/admin/sala/{id}/lugares/")
     public ModelAndView listaLugares(@PathVariable("id") Integer id) {
 
@@ -82,6 +97,7 @@ public class SalaController {
         return modelAndView;
     }
 
+    
 
     @DeleteMapping("/admin/sala/{id}")
     @ResponseBody
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,25 +2,42 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
-	
+
+		@Id
+		@GeneratedValue
 		private Integer id;
 		private LocalTime horario;
+		
+		@ManyToOne
 		private Sala sala;
+		
+		@ManyToOne
 		private Filme filme;
+		
+		/**
+		 * @deprecated hibernate only
+		 */
+		public Sessao(){}
 		
 		public Sessao (	LocalTime horario,
 						Filme filme,
 						Sala sala){
 		
 			this.horario = horario;
-			this.filme = filme;
+			this.setFilme(filme);
 			this.sala = sala;
 		}
 		
 		public LocalTime getHorarioTermino(){
 		
-			return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+			return this.horario.plusMinutes(getFilme().getDuracao().toMinutes());
 		}
 		
 		public void setId (Integer id){
@@ -43,8 +60,14 @@ public class Sessao {
 		public Sala getSala (){
 			return this.sala;
 		}
-		
-		public void setFilme (Filme filme){
+
+		public Filme getFilme() {
+			return filme;
+		}
+
+		public void setFilme(Filme filme) {
 			this.filme = filme;
 		}
+		
+	
 }
